8a9ea631bf587aabf17b97582706d02265b7c7e3
yui kim <kyounghee_kim@rapid7.com>
2022-07-26 21:23:23 -0400

    fix not to mutates state using spread operator

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,6 @@
 export default function courseReducer(state = [], action) {
   switch (action.type) {
     case "CREATE_COURSE":
-      state.push(action.course); // todo: don't use push - mutates state
+      return [...state, { ...action.course }];
   }
 }
