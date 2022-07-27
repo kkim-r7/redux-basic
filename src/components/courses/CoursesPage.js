4a91a0360f6e3a8de2802d400ec9d21def9271c3
yui kim <kyounghee_kim@rapid7.com>
2022-07-26 20:05:15 -0400

    add bind for func but need to change the code

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,34 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+  }
+
+  handleChange(event) {
+    debugger;
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  }
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form>
+        <h2>Course</h2>
+        <h4>Add Course</h4>
+        <input
+          type="text"
+          onChange={this.handleChange.bind(this)} // new function is allocated on every render so need to change
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+      </form>
+    );
   }
 }
 
