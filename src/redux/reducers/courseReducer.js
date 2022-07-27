3c11e0054fdc2f46ccc38bf1c5ad9c448b82a9a6
yui kim <kyounghee_kim@rapid7.com>
2022-07-26 21:30:56 -0400

    create rootReducer using combineReducers of redux

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -2,5 +2,7 @@ export default function courseReducer(state = [], action) {
   switch (action.type) {
     case "CREATE_COURSE":
       return [...state, { ...action.course }];
+    default:
+      return state;
   }
 }
